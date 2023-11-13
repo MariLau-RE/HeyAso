669fa510a8b31b4f8fecf0af9cc3b7b6663bc626
Mariana <mfernandezmar3@gmail.com>
2023-11-13 17:13:27 -0600

    preguntas foro listooo

--- Backend/foro/preguntasForo.js
+++ Backend/foro/preguntasForo.js
@@ -1,5 +1,66 @@
-import { collection, getDocs, addDoc } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+import { collection, getDocs, addDoc, serverTimestamp  } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 import { db } from "../configDatabase.js"
 
-const comentarios = collection(db, 'Comentario');
-const listaComentario = await getDocs(comentarios);
+const comentarios = collection(db, 'Comentarios');
+const usuarios = collection(db, 'Usuarios');
+
+const submitButton = document.getElementById("submit-button");
+const commentInput = document.getElementById("comment-input");
+const commentsContainer = document.getElementById("comments-container");
+
+document.addEventListener("DOMContentLoaded", function() {
+    cargarComentarios();
+});
+
+async function cargarComentarios() {
+    const listaComentario = await getDocs(comentarios);
+    
+    if(listaComentario.size != 0){
+        listaComentario.forEach(doc =>{
+            const comment = doc.data().usuario+": "+doc.data().mensaje;
+            const commentElement = document.createElement("div");
+            commentElement.innerHTML = comment;
+            commentElement.style.textAlign = "left";
+            commentsContainer.appendChild(commentElement);
+        });
+    }
+}
+
+
+submitButton.addEventListener("click", async function() {
+    var carnet = localStorage.getItem('carnet');
+    var nombreUsuario;
+
+    const listaUsuario = await getDocs(usuarios);
+
+    listaUsuario.forEach(docUs =>{
+        if(docUs.data().carnet == carnet){
+            nombreUsuario = docUs.data().nombre;
+        }
+    })
+
+    const comment = nombreUsuario+": "+commentInput.value;
+    const commentElement = document.createElement("div");
+    commentElement.innerHTML = comment;
+    commentElement.style.textAlign = "right";
+    commentsContainer.appendChild(commentElement);
+    commentInput.value = "";
+
+
+
+    // Crea un nuevo timestamp con la fecha y hora actuales del servidor
+    var miTimestamp = serverTimestamp();
+
+    try {
+        const docRef = await addDoc(comentarios, {
+          usuario: nombreUsuario,
+          mensaje: comment,
+          timestamp: miTimestamp
+        });
+        console.log("Comentario creado con ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error al agregar el documento: ", e);
+    }
+});
+
+
