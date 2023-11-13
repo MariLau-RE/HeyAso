2e11d9ccc5c193004e603ae078c822b681c9d9de
Mariana <mfernandezmar3@gmail.com>
2023-11-13 15:15:53 -0600

    eliminar colaborador listo

--- Backend/administrarColaborador/eliminarColaborador.js
+++ Backend/administrarColaborador/eliminarColaborador.js
@@ -1,63 +1,76 @@
-import { collection, setDoc, getDocs, doc } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+import { collection, getDocs, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 import { db } from "../configDatabase.js"
 
 const usuarios = collection(db, 'Usuarios');
+const asociaciones = collection(db, 'Asociacion');
 const listaUsuario = await getDocs(usuarios);
+const listaAsociacion = await getDocs(asociaciones);
 
-var select = document.getElementById("selectEvento");
+var usuSelect = document.getElementById("colaborador");
+var asoSelect = document.getElementById("asociacion");
 
-listaUsuario.docs.forEach(doc => {
-    // Creas un nuevo elemento option
-    var option = document.createElement("option");
+asoSelect.addEventListener('change', function() {
+  var asoSelect = this.value;
+  console.log(asoSelect);
 
-    // Le asignas un valor y un texto
-    option.value = doc.data().carnet;
-    option.text = doc.data().nombre;
+  // Elimina todas las opciones existentes, excepto la primera
+  while (usuSelect.options.length > 1) {
+    usuSelect.remove(1);
+  }
 
-    // Agregas la opción al select
-    select.appendChild(option);
-});
-
-/*FALTA CONECTAR AL FRONT */
-
-async function eliminarColaborador() {
-  var carnet = select.value;
-  var nombre, carrera, correo, contrasenna, contacto, descripcion, idAsociacion, puesto;
-
-  if(carnet == '0'){
-    alert("Debe seleccionar un estudiante.");
+  if(asoSelect == '0'){
+    usuSelect.value = '0';
   }else{
-    
-    listaUsuario.docs.forEach(doc => {
-      if(doc.data().carnet == carnet){
-          nombre = doc.data().nombre;
-          carrera = doc.data().carrera;
-          correo = doc.data().correo;
-          contrasenna = doc.data().contraseña;
-          contacto = doc.data().contacto;
-          descripcion = doc.data().descripcion;
-          idAsociacion = doc.data().idAsociacion;
-          puesto = doc.data().puesto;
+    listaUsuario.docs.forEach(docUs => {
+      if(docUs.data().idTipo == "Admin" && docUs.data().idAsociacion == asoSelect){
+          // Creas un nuevo elemento option
+          var option = document.createElement("option");
+
+          // Le asignas un valor y un texto
+          option.value = docUs.data().carnet;
+          option.text = docUs.data().nombre;
+        
+          // Agregas la opción al select
+          usuSelect.appendChild(option);
       }
     });
+  }
 
+});
+
+
+listaAsociacion.docs.forEach(doc => {
+  // Creas un nuevo elemento option
+  var option = document.createElement("option");
+
+  // Le asignas un valor y un texto
+  option.value = doc.data().idAsociacion;
+  option.text = doc.data().nombre + " - " + doc.data().idAsociacion;
+
+  // Agregas la opción al select
+  asoSelect.appendChild(option);
+});
+
+
+async function eliminarColaborador() {
+  var carnet = usuSelect.value;
+  var asocia = asoSelect.value;
+
+  if(asocia == '0' || carnet == '0'){
+    alert("Debe seleccionar una asociación y un colaborador.");
+  }else{
     try {
-      await setDoc(doc(usuarios, carnet), {
-        nombre: nombre,
-        carrera: carrera,
-        carnet: carnet,
-        correo: correo,
-        contraseña: contrasenna,
-        contacto: contacto,
-        descripcion: descripcion,
-        idTipo: "Estudiante",
-        idAsociacion: idAsociacion,
-        puesto: puesto
-      });
-      console.log("Colaborador eliminado con ID: ", carnet);
-      alert("Colaborador eliminado con éxito");
+      const docRef = doc(usuarios, carnet); // Obtiene la referencia al documento
+      await updateDoc(docRef, { 
+          idAsociacion: "",
+          idTipo: "Estudiante",
+          puesto: ""
+       });
+       console.log("Colaborador eliminado/actualizado");
+       alert("Colaborador eliminado con éxito");
+       window.location.href="../PaginaPrincipal.html";
     } catch (e) {
-      console.error("Error al agregar el documento: ", e);
+      console.error("Error al actualizar el documento: ", e);
     }
   }
 }
--- Backend/administrarColaborador/modificarColaborador.js
+++ Backend/administrarColaborador/modificarColaborador.js
@@ -1,23 +1,83 @@
-import { collection, setDoc, getDocs, doc } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+import { collection, updateDoc, getDocs, doc } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 import { db } from "../configDatabase.js"
 
 const usuarios = collection(db, 'Usuarios');
+const asociaciones = collection(db, 'Asociacion');
 const listaUsuario = await getDocs(usuarios);
+const listaAsociacion = await getDocs(asociaciones);
 
-var select = document.getElementById("selectEvento");
+var select = document.getElementById("colaboradores");
+var puestoSelect = document.getElementById("puesto");
+var asoSelect = document.getElementById("asociacion");
 
-listaUsuario.docs.forEach(doc => {
+select.addEventListener('change', function() {
+    var usuSelect = this.value;
+    console.log(usuSelect);
+  
+    if(usuSelect == '0'){
+      document.getElementById("nombre").value = '';
+      document.getElementById("carrera").value = '';
+      document.getElementById("puesto").value = '0';
+      document.getElementById("asociacion").value = '0';
+      document.getElementById("correo").value = '';
+      document.getElementById("contacto").value = '';
+    }else{
+      listaUsuario.docs.forEach(docUs => {
+        if(docUs.data().carnet == usuSelect){
+            document.getElementById("nombre").value = docUs.data().nombre;
+            document.getElementById("carrera").value = docUs.data().carrera;
+            puestoSelect.value = docUs.data().puesto;
+            asoSelect.value = docUs.data().idAsociacion;
+            document.getElementById("correo").value = docUs.data().correo;
+            document.getElementById("contacto").value = docUs.data().contacto;
+        }
+      });
+    }
+});
+
+listaAsociacion.docs.forEach(doc => {
     // Creas un nuevo elemento option
     var option = document.createElement("option");
 
     // Le asignas un valor y un texto
-    option.value = doc.data().carnet;
-    option.text = doc.data().nombre;
+    option.value = doc.data().idAsociacion;
+    option.text = doc.data().nombre + " - " + doc.data().idAsociacion;
 
     // Agregas la opción al select
-    select.appendChild(option);
+    asoSelect.appendChild(option);
 });
 
-/*FALTA MOSTRAR LA INFORMACION DE LA BUSQUEDA
-TOMAR LOS VALORES CAMBIADOS Y HACER EL UPDATE
-CONECTAR AL FRONT */
+listaUsuario.docs.forEach(doc => {
+    if(doc.data().idTipo == "Admin"){
+        // Creas un nuevo elemento option
+        var option = document.createElement("option");
+
+        // Le asignas un valor y un texto
+        option.value = doc.data().carnet;
+        option.text = doc.data().nombre;
+
+        // Agregas la opción al select
+        select.appendChild(option);
+    }
+});
+
+async function modificarColab(){
+    var colaborador = select.value;
+    var puesto = puestoSelect.value;
+    var asocia = asoSelect.value;
+
+    try {
+        const docRef = doc(usuarios, colaborador); // Obtiene la referencia al documento
+        await updateDoc(docRef, { 
+          puesto: puesto,
+          idAsociacion: asocia
+         });
+         console.log("Colaborador actualizado con ID: ", colaborador);
+         alert("Colaborador actualizado con éxito");
+         window.location.href="../AdministrarColaborador.html";
+      } catch (e) {
+        console.error("Error al actualizar el documento: ", e);
+      }
+}
+
+window.modificarColab = modificarColab;
