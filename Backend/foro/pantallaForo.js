07ffc77337436d733b347c6a686a2ecd83d24d54
aleedca <acerdas1701@estudiantec.cr>
2023-11-15 12:00:22 -0600

    correcciones finales

--- Backend/foro/pantallaForo.js
+++ Backend/foro/pantallaForo.js
@@ -1,24 +1,27 @@
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 import { db } from "../configDatabase.js"
 
-const usuarios = collection(db, 'Usuarios');
-const listaUsuario = await getDocs(usuarios);
-var btnProcesar = document.getElementById("btn_procesar");
-var carnet = localStorage.getItem('carnet');
-
-
 document.addEventListener("DOMContentLoaded", async function() {
+    const usuarios = collection(db, 'Usuarios');
+    const listaUsuario = await getDocs(usuarios);
+    var btnProcesar = document.getElementById("btn_procesar");
+    var btnEvaluar = document.getElementById("btn_evaluar");
+    var carnet = localStorage.getItem('carnet');
     listaUsuario.forEach(docUs =>{
         if(docUs.data().carnet == carnet){
             if(docUs.data().idTipo == "Admin"){
                 btnProcesar.style.display = "block";
+                btnEvaluar.style.display = "none";
             }
         }
     });
 });
 
 
-function menu(){
+async function menu(){
+    const usuarios = collection(db, 'Usuarios');
+    const listaUsuario = await getDocs(usuarios);
+    var carnet = localStorage.getItem('carnet');
     listaUsuario.forEach(docUs =>{
         if(docUs.data().carnet == carnet){
             if(docUs.data().idTipo == "Admin"){
